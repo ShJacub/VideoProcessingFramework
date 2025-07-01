9ba880e099a3c6c39c24e38c4c24712beb05078f
Yakub <sharafutdinov.yan@gosniias.ru>
2025-07-01 16:45:42 +0300

    feat:add python script for torch version setting in setup.py and pyproject.toml

--- README.md
+++ README.md
@@ -3,5 +3,4 @@ Original repository - https://github.com/NVIDIA/VideoProcessingFramework
 
 ## Problem  
 VideoProcessingFramework is compiled with latest version of torch and doesn't work with previous versions.  
-For this to be possible it's necessary to bound or set properly the version of torch in setup.py and  
-pyproject.toml files
+For this to be possible it's necessary to bound or set properly the version of torch in setup.py and pyproject.toml files with setup_torch_version.py  
---
+++ setup_torch_version.py
@@ -0,0 +1,25 @@
+import torch.__version__ as torch_version
+torch_version = torch_version.__str__().split('+')[0]
+
+setup_file_path = "src/PytorchNvCodec/setup.py"
+setup_script = []
+with open(setup_file_path, "r") as fopen:
+    for line in fopen:
+        if 'install_requires=["torch"]' in line:
+            line = line.replace('"torch"', f'"torch<={torch_version}"')
+        setup_script.append(line)
+with open(setup_file_path, "w") as fopen:
+    setup_script = "".join(setup_script)
+    fopen.write(setup_script)
+
+
+pyproject_file_path = "src/PytorchNvCodec/pyproject.toml"
+pyproject_text = []
+with open(pyproject_file_path, "r") as fopen:
+    for line in fopen:
+        if '"torch"' in line:
+            line = line.replace('"torch"', f'"torch<={torch_version}"')
+        pyproject_text.append(line)
+with open(pyproject_file_path, "w") as fopen:
+    pyproject_text = "".join(pyproject_text)
+    fopen.write(pyproject_text)
